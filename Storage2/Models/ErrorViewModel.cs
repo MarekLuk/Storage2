81e8d7ba9478a1263446dceba0be73c626718768
MarekL-MarekL <markandmark3@gmail.com>
2024-04-15 01:24:34 +0200

    Add project files.

---
+++ Storage2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Storage2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
