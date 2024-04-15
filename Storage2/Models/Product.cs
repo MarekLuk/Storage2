40c46d380ed0345742dca9a617a0979587b58735
MarekL-MarekL <markandmark3@gmail.com>
2024-04-15 08:57:30 +0200

    next

--- Storage2/Models/Product.cs
+++ Storage2/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace Storage2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage2.Models
 {
     public class Product
     {
@@ -6,10 +8,16 @@
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        [Range(1, 1000000)]
         public int Price { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime Orderdate { get; set; }
         public string Category { get; set; }
         public string Shelf { get; set; }
+
+        [Range(1, 10000)]
         public int Count { get; set; }
         public string Description { get; set; }
 
